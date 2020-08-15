5550abc471265b4ee4ffb7175c939b81aba20963
Abby <alope096@ucr.edu>
2020-08-15 01:30:00 -0700

    part4

---
+++ turnin/alope096_lab4_part3.c
@@ -0,0 +1,75 @@
+/*	Author: lab
+ *  Partner(s) Name: None
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {locked, buttonHashPressed, buttonHashrelease, buttonYpress, insideButton} state;
+
+void tick(){
+   unsigned char button_Y = PINA & 0x02;
+   unsigned char button_H = PINA & 0x04;
+   unsigned char button_PA7 = PINA & 0x80;
+   unsigned char lockedUnlocked;
+   
+   switch(state){
+      case locked:
+         state = button_H? buttonHashPressed : locked;
+      break;   
+      case buttonHashPressed:
+         state = button_H? buttonHashPressed : buttonHashrelease;
+      break; 
+      case buttonHashrelease:
+         state = button_Y? buttonYpress : buttonHashrelease;
+      break;
+      case buttonYpress:
+         state = button_PA7? insideButton : buttonYpress;
+      break; 
+      case insideButton:
+         state = locked;
+      break;
+      default:
+         state = locked;
+      break;
+   }
+   switch(state){
+      case locked:
+         lockedUnlocked = 0x00; 
+      break;   
+      case buttonHashPressed:
+      break; 
+      case buttonHashrelease:
+      break; 
+      case buttonYpress:
+         lockedUnlocked = 0x01; 
+      break;
+      case insideButton:
+         lockedUnlocked = 0x00; 
+      break;
+   }
+   PORTB = lockedUnlocked;
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+   DDRA=0X00;  PORTA=0XFF;
+   DDRB=0XFF;  PORTB=0X00;
+   
+
+    /* Insert your solution below */
+   state = locked;
+
+   while (1) {
+     tick();
+   }
+    return 1;
+   
+}
